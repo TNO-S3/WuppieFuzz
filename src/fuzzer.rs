a2f52413d8d36c58d545109d17c9e7f860e8c5e6
Thomas Rooijakkers <thomas.rooijakkers@tno.nl>
2024-11-08 19:12:32 +0100

    Ugly fix

--- src/fuzzer.rs
+++ src/fuzzer.rs
@@ -336,9 +336,18 @@ pub fn fuzz() -> Result<()> {
         .map(|timeout| Instant::now() - starting_time < timeout)
         .unwrap_or(true)
     {
-        fuzzer
-            .fuzz_one(&mut stages, &mut executor, &mut state, &mut mgr)
-            .context("Error in the fuzzing loop")?;
+        match fuzzer
+            .fuzz_one(&mut stages, &mut executor, &mut state, &mut mgr) {
+                Ok(res) => res,
+                Err(err) => match err {
+                    libafl_bolts::Error::ShuttingDown => {
+                        return Ok(());
+                    }
+                    _ => {
+                        panic!("Error in the fuzz loop.");
+                    }
+                }
+            };
         // send update of execution data to the monitor
         let executions = *state.executions();
         if let Err(e) = mgr.fire(
